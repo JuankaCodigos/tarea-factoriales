4a2601a9b42b01490cce46780b2b27b795c06c9a
JuankaCodigos <j.vega18@ufromail.cl>
2026-08-26 13:57:48 -0400

    Add files via upload

---
+++ Maincon_reposicion.java
@@ -0,0 +1,25 @@
+package org.example;
+import java.util.Scanner;
+
+public class Maincon_reposicion {
+    static Scanner entrada = new Scanner(System.in);
+    public static long factorial(int numero) {
+        long resultado = 1;
+        for (int i = 1; i <= numero; i++) {
+            resultado = resultado * i;
+        }
+        return resultado;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Ingrese el numero que desea calcular");
+        int numero = entrada.nextInt();
+        long TIEMPO_PARTIDA = System.nanoTime();
+        long resultado = factorial(numero);
+        long TIEMPO_TERMINO = System.nanoTime();
+        System.out.println("Factorial: " + resultado);
+        double tiempototal =
+                (TIEMPO_TERMINO - TIEMPO_PARTIDA) / 1000000.0;
+        System.out.println("Tiempo " + tiempototal + " milisegundo");
+    }
+}
