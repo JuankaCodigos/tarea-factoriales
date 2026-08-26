060e74cf7adf25a17f4ab8aea41542909c16361e
JuankaCodigos <j.vega18@ufromail.cl>
2026-08-26 13:56:33 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,25 +1,25 @@
 package org.example;
 import java.util.Scanner;
+
 public class Main {
     static Scanner entrada = new Scanner(System.in);
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("Ingrese el numero que desea calcular");
-        int numero=entrada.nextInt();
-        int factorialacum=1;
-        long TIEMPO_PARTIDA= System.nanoTime();
-for(int i=1; i<=numero;i++){
-    factorialacum=factorialacum*i;
-}
-        long TIEMPO_TERMINO= System.nanoTime();
-        System.out.println(factorialacum);
-        double tiempototal=(TIEMPO_TERMINO-TIEMPO_PARTIDA)/1000000.0;
-        System.out.println("Tiempo "+tiempototal+" milisegundo");
+    public static long factorial(int numero) {
+        long resultado = 1;
+        for (int i = 1; i <= numero; i++) {
+            resultado = resultado * i;
+        }
+        return resultado;
     }
 
-
-
-
-
-
+    public static void main(String[] args) {
+        System.out.println("Ingrese el numero que desea calcular");
+        int numero = entrada.nextInt();
+        long TIEMPO_PARTIDA = System.nanoTime();
+        long resultado = factorial(numero);
+        long TIEMPO_TERMINO = System.nanoTime();
+        System.out.println("Factorial: " + resultado);
+        double tiempototal =
+                (TIEMPO_TERMINO - TIEMPO_PARTIDA) / 1000000.0;
+        System.out.println("Tiempo " + tiempototal + " milisegundo");
+    }
 }
