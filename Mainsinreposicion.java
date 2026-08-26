faef6fc5ec04e3590802d98955ed8aaf81a6943d
JuankaCodigos <j.vega18@ufromail.cl>
2026-08-26 13:58:27 -0400

    Add files via upload

---
+++ Mainsinreposicion.java
@@ -0,0 +1,25 @@
+package org.example;
+import java.util.Scanner;
+public class Mainsinreposicion {
+    static Scanner entrada = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Ingrese el numero que desea calcular");
+        int numero=entrada.nextInt();
+        int factorialacum=1;
+        long TIEMPO_PARTIDA= System.nanoTime();
+for(int i=1; i<=numero;i++){
+    factorialacum=factorialacum*i;
+}
+        long TIEMPO_TERMINO= System.nanoTime();
+        System.out.println(factorialacum);
+        double tiempototal=(TIEMPO_TERMINO-TIEMPO_PARTIDA)/1000000.0;
+        System.out.println("Tiempo "+tiempototal+" milisegundo");
+    }
+
+
+
+
+
+
+}
